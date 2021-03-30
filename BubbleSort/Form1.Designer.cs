a21710eb236947e1ea69c921346ff6fd2c074877
vpaskova <viara_paskova@abv.bg>
2021-03-30 10:32:04 +0300

    Commit all files

---
+++ BubbleSort/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System.Drawing;
+
+namespace BubbleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(16, 82);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1645, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(308, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(807, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1143, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сортирай";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.AliceBlue;
+            this.label1.Font = new System.Drawing.Font("Arial", 35F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.BurlyWood;
+            this.label1.Location = new System.Drawing.Point(17, 258);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 69);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(31, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Въведете масив от числа:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1669, 437);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "BubbleSort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BubbleSort/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSort
+{
+    public partial class Form1 : Form
+    {
+        //Списък на всички бутончета за всяко число
+        List<Button> buttons = new List<Button>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Тук влиза когато се натисне бутона "Сортирай"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Зачистваме за всеки случай надписа 
+            label1.BeginInvoke((MethodInvoker)delegate () { label1.Text = ""; });
+
+            //Създаваме си един масив от стрингове за да вземем въведените данни в textbox-a
+            string[] arrString = textBox1.Text.Split(',');
+            //Проверка ако случайно нищо не е въведено да не гърми, а да покаже съобщение
+            if (arrString.Length <= 1)
+            {
+                MessageBox.Show("Моля въведете коректен масив от числа!");
+                return;
+            }
+            //Конвертираме си данните в масив от int За да работим с числа 
+            int[] arr = Array.ConvertAll(arrString, s => int.Parse(s));
+
+            //Обхождаме всяко число и му създаваме бутонче извиквайки за всяко число метода CreateButton
+            for (int i = 0; i < arr.Length; i++)
+                CreateButton(arr[i].ToString(), i);
+
+            //След като сме начертали бутончетата сортираме масива
+            BubbleSort(arr);
+        }
+
+        //Метод за създаване на бутонче за всяко число, за да може да се оцветяват и да показват как се извършва алгоритъма
+        private void CreateButton(string text, int index)
+        {
+            //Създаваме бутон, даваме му височина и ширина и за локация изполваме индекса, за да го умножим по 70 и бутончетата да не се застъпват
+            Button btn = new Button();
+            btn.Height = 40;
+            btn.Width = 60;
+            btn.Location = new Point(index * 70, 0);
+            btn.Text = text;
+            btn.Name = index.ToString();
+            //Тук добавяме бутона в панела
+            //BeginInvoke((MethodInvoker)delegate () това се използва понеже добавяме в този панел много пъти и основната нишка по този начин се достъпва много пъти
+            //а не може да се и гърми и по този начин се изчаква да се завърши добавянето на бутона в панела преди да се премине към следващо добавяне
+            // това с BeginInvoke го има и надолу логиката е същата - ползва се за да се завърши дадено действие във визуалната част
+            panel1.BeginInvoke((MethodInvoker)delegate () { panel1.Controls.Add(btn); });
+            //Добавяме си бутона в списък, за да го ползваме и в другите методи като се оцветяват
+            buttons.Add(btn);
+        }
+
+        //Това е самият алгоритъм
+        public void BubbleSort(int[] array)
+        {
+            int temp;
+            //Алгоритъма го стартираме в отделна нишка с този ред като идеята е да може да я приспиваме
+            //, за да се създаде тази анимация при обхождането и да се види точно какво прави алгоритъма
+            Task.Factory.StartNew(() =>
+            {
+                //Приспиваме нишката за 2 секунди
+                Thread.Sleep(2000);
+                for (int j = 0; j <= array.Length - 2; j++)
+                {
+                    for (int i = 0; i <= array.Length - 2; i++)
+                    {
+                        //с тези редове оцветяваме в кафяво полетата, които се преглеждат в момента от алгоритъма
+                        buttons[i].BackColor = Color.Chocolate;
+                        Thread.Sleep(200);
+                        buttons[i + 1].BackColor = Color.Chocolate;
+                        Thread.Sleep(500);
+
+                        //Тук влиза ако трябва да размени двете стойности (т.е. ако първото число е по-голямо от второто)
+                        if (array[i] > array[i + 1])
+                        {
+                            temp = array[i + 1];
+                            array[i + 1] = array[i];
+                            array[i] = temp;
+
+                            //Щом ще ги разменя ги оцветяваме в червено, за да си личи кога се случва нещо
+                            buttons[i].BeginInvoke((MethodInvoker)delegate () { buttons[i].Text = temp.ToString(); });
+                            buttons[i].BackColor = Color.Red;
+                            buttons[i + 1].BeginInvoke((MethodInvoker)delegate () { buttons[i + 1].Text = array[i + 1].ToString(); });
+                            buttons[i + 1].BackColor = Color.Red;
+                            Thread.Sleep(500);
+
+                        }
+                        //След като е свършило с проверката на двете числа ги оцветяваме пак в бяло, за да преминем към следващите
+                        buttons[i].BackColor = Color.AntiqueWhite;
+                        buttons[i + 1].BackColor = Color.AntiqueWhite;
+                    }
+                }
+                //Когато е готово сортирането изписваме надпис отдолу със вече сортирания масив
+                label1.BeginInvoke((MethodInvoker)delegate () { label1.Text = "Сортираният масив е :\n" + string.Join(",", array); });
+            });
+        }        
+    }
+}
